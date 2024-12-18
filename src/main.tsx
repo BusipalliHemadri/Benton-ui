6a1dd4f7bb4fb77c4ceaab7ff79de5b3791c08e5
Hemadri Busipalli <bhemadri@syncorsol.com>
2024-12-18 19:11:11 +0530

    Updated reverts

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { Toaster } from './components/ui/toaster';
-import { FrappeProvider } from 'frappe-react-sdk';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FrappeProvider url="https://erp-dev.leremitt.com/">
-      <App />
-      <Toaster />
-    </FrappeProvider>
+    <App />
+    <Toaster />
   </StrictMode>
 );
