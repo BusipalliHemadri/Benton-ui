7ee466e5355517d0426c9e13a1b721c7b15d6a92
Hemadri Busipalli <bhemadri@syncorsol.com>
2024-12-18 15:48:37 +0530

    update frappe provieder url

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FrappeProvider url="http://erp-dev.leremitt.com/">
+    <FrappeProvider url="https://erp-dev.leremitt.com/">
       <App />
       <Toaster />
     </FrappeProvider>
